68129e86512b8323aa32b0b449e33b8bf181ef23
Robson Silva <rms2307@outlook.com>
2020-08-07 20:15:27 -0300

    definindo layout basico

---
+++ app/src/main/java/com/rms2307/minhastarefas/AdicionarTarefaActivity.java
@@ -0,0 +1,14 @@
+package com.rms2307.minhastarefas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdicionarTarefaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adicionar_tarefa);
+    }
+}
