329e2afee128d25f6da6ac48b0b113f7ef6fb1f0
Robson Silva <rms2307@outlook.com>
2020-08-09 20:24:48 -0300

    adicionando menu

--- app/src/main/java/com/rms2307/minhastarefas/activity/AdicionarTarefaActivity.java
+++ app/src/main/java/com/rms2307/minhastarefas/activity/AdicionarTarefaActivity.java
@@ -1,8 +1,11 @@
 package com.rms2307.minhastarefas.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.rms2307.minhastarefas.R;
 
@@ -12,5 +15,21 @@ public class AdicionarTarefaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adicionar_tarefa);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_adicionar_tarefa, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.itemSalvar:
+                
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
